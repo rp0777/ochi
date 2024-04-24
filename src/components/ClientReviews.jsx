46d2c9db98fee7879e1ea096b74a48068084d46f
unknown <rp0777>
2024-04-24 16:33:57 +0530

    feat : Implement Featured, Client Reviews and Footer component

--- src/components/ClientReviews.jsx
+++ src/components/ClientReviews.jsx
@@ -1,5 +1,51 @@
 function ClientReviews() {
-  return <div>ClientReviews</div>;
+  return (
+    <section
+      data-scroll
+      data-scroll-section
+      data-scroll-speed="0.5"
+      className=" w-full h-full px-[8vw] lg:px-32 py-16 bg-zinc-800"
+    >
+      <h1 className=" text-3xl mb-7">Client&apos;s Reviews</h1>
+
+      <div className="  flex flex-col lg:flex-row items-center gap-5">
+        <div className=" cardcontainer h-[50vh] w-full lg:w-1/2">
+          {/* Card 1 - Ochi */}
+          <div className=" card relative rounded-xl w-full h-full bg-[#004d43] flex justify-center items-center">
+            <img src="/ochi.svg" alt="ochi logo" />
+
+            <button className=" absolute bottom-10 text-xs left-10 px-5 py-2 border-2 rounded-full border-[#CDEA68] text-[#CDEA68]">
+              &copy;2019-2022
+            </button>
+          </div>
+        </div>
+
+        <div className=" cardcontainer flex flex-col lg:flex-row gap-5 h-full lg:h-[50vh] w-full lg:w-1/2">
+          {/* Card 2 - Clutch */}
+          <div className=" card relative rounded-xl w-full lg:w-1/2 h-[40vh] lg:h-full bg-[#3e3d3d] flex justify-center lg:items-center items-start pt-10 lg:pt-0">
+            <img src="/clutch.svg" alt="clutch logo" />
+
+            <button className=" absolute bottom-10 text-xs left-10 px-5 py-2 border-2 rounded-full border-[#CDEA68] text-[#CDEA68]">
+              RATING 5.0 ON CLUTCH
+            </button>
+          </div>
+
+          {/* Card 3 - The Futur Academy */}
+          <div className=" card relative rounded-xl w-full lg:w-1/2 h-[40vh] lg:h-full bg-[#3e3d3d] flex justify-center lg:items-center items-start">
+            <img
+              className=" scale-50 rounded-full"
+              src="/future.png"
+              alt="future academy logo"
+            />
+
+            <button className=" absolute bottom-10 text-xs left-10 px-5 py-2 border-2 rounded-full border-[#CDEA68] text-[#CDEA68]">
+              BUSINESS BOOTCAMP ALUMNI
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default ClientReviews;
--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -1,5 +1,112 @@
+import { motion, useAnimation } from "framer-motion";
+
 function Featured() {
-  return <div>Featured</div>;
+  const cards = [useAnimation(), useAnimation()];
+
+  const handleHover = (index) => {
+    cards[index].start({ y: "0" });
+  };
+
+  const handleHoverEnd = (index) => {
+    cards[index].start({ y: "100%" });
+  };
+
+  return (
+    <div
+      data-scroll
+      data-scroll-section
+      className=" w-full lg:py-10 mb-10 lg:mb-0"
+    >
+      {/* Main Heading */}
+      <div className=" w-full px-[6vw] lg:px-20 border-b-[1px] border-zinc-700 py-4 lg:py-0 lg:pb-20">
+        <h1 className=" text-[8vw] lg:text-[3vw]">Featured projects</h1>
+      </div>
+
+      <div className=" px-[6vw] lg:px-20">
+        <div className=" cards w-full flex flex-col lg:flex-row gap-10 mt-10">
+          {/* Card 1 - Fyde */}
+          <motion.div
+            onHoverStart={() => handleHover(0)}
+            onHoverEnd={() => handleHoverEnd(0)}
+            className=" relative cardcontainer w-full lg:w-1/2 h-full lg:h-[42vw]"
+          >
+            {/* Card Heading */}
+            <div className=" flex justify-start items-center p-4 gap-4">
+              <div className="dot bg-white size-2 rounded-full"></div>
+              <h1 className=" uppercase">Fyde</h1>
+            </div>
+
+            {/* Card Title with hover effect */}
+            <h1 className=" absolute hidden lg:flex overflow-hidden text-[#CDEA68] left-full top-1/2 -translate-x-1/2 -translate-y-1/2 z-[9] leading-none tracking-tighter text-8xl font-bold">
+              {"FYDE".split("").map((item, index) => (
+                <motion.span
+                  initial={{ y: "100%" }}
+                  animate={cards[0]}
+                  transition={{
+                    ease: [0.22, 1, 0.36, 1],
+                    delay: index * 0.054,
+                  }}
+                  className=" inline-block translate-y-full"
+                  key={index}
+                >
+                  {item}
+                </motion.span>
+              ))}
+            </h1>
+
+            {/* Card background Image */}
+            <div className=" card w-full h-full lg:h-[38vw] overflow-hidden">
+              <img
+                className=" hover:scale-95 transition-all rounded-xl w-full h-full bg-cover"
+                src="/project1.png"
+                alt=""
+              />
+            </div>
+          </motion.div>
+
+          {/* Card 1 - Vise */}
+          <motion.div
+            onHoverStart={() => handleHover(1)}
+            onHoverEnd={() => handleHoverEnd(1)}
+            className=" relative cardcontainer w-full lg:w-1/2 h-full lg:h-[42vw]"
+          >
+            {/* Card Heading */}
+            <div className=" flex justify-start items-center p-4 gap-4">
+              <div className="dot bg-white size-2 rounded-full"></div>
+              <h1 className=" uppercase">Vise</h1>
+            </div>
+
+            {/* Card Title with hover effect */}
+            <h1 className=" absolute hidden lg:flex overflow-hidden text-[#CDEA68] right-full top-1/2 translate-x-1/2 -translate-y-1/2 z-[9] leading-none tracking-tighter text-8xl font-bold">
+              {"VISE".split("").map((item, index) => (
+                <motion.span
+                  initial={{ y: "100%" }}
+                  animate={cards[1]}
+                  transition={{
+                    ease: [0.22, 1, 0.36, 1],
+                    delay: index * 0.054,
+                  }}
+                  className=" inline-block translate-y-full"
+                  key={index}
+                >
+                  {item}
+                </motion.span>
+              ))}
+            </h1>
+
+            {/* Card background Image */}
+            <div className=" card w-full h-full lg:h-[38vw] overflow-hidden">
+              <img
+                className=" hover:scale-95 transition-all rounded-xl w-full h-full bg-cover"
+                src="/project2.png"
+                alt=""
+              />
+            </div>
+          </motion.div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Featured;
